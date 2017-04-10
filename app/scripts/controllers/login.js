423be8842d17ddf47b1ffc6be0bf45b3ce10323c
ErmiasG <ermiasg@kth.se>
2017-04-10 18:45:58 +0200

    login and project pages

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,10 +8,11 @@
  * Controller of the dashboardApp
  */
 angular.module('dashboardApp')
-  .controller('LoginCtrl', function () {
+  .controller('LoginCtrl',['$rootScope', function ($rootScope) {
+    $rootScope.logedIn = false;
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+  }]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,15 @@
  * Controller of the dashboardApp
  */
 angular.module('dashboardApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl',['$rootScope','growl', function ($rootScope, growl) {
+    $rootScope.logedIn = true;
+    $rootScope.projectId = 0;
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+    growl.success("This is a test for success.", {title: 'Success', ttl: 500, referenceId: 1});
+    growl.error("This is a test for error.", {title: 'Error', ttl: 500, referenceId: 1});
+    growl.warning("I'm the warning message", {title: 'Warning', ttl: 500, referenceId: 1});
+  }]);
