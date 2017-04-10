c4570e87c192e8cbc842181faab18c1746743516
ErmiasG <ermiasg@kth.se>
2017-04-10 18:52:42 +0200

    repeat projects

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,10 +11,19 @@ angular.module('dashboardApp')
   .controller('MainCtrl',['$rootScope','growl', function ($rootScope, growl) {
     $rootScope.logedIn = true;
     $rootScope.projectId = 0;
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    this.projects = [
+      'Project1',
+      'Project2',
+      'Project3',
+      'Project4',
+      'Project5',
+      'Project6',
+      'Project7',
+      'Project8',
+      'Project9',
+      'Project10',
+      'Project11',
+      'Project12'
     ];
     growl.success("This is a test for success.", {title: 'Success', ttl: 500, referenceId: 1});
     growl.error("This is a test for error.", {title: 'Error', ttl: 500, referenceId: 1});
